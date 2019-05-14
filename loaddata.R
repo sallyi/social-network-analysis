66f8751f59fcc99b6d99544c33d6957a5c1c1aaa
Sally Isaacoff <sally.isaacoff@gmail.com>
2019-05-14 16:23:53 +0200

    delete old files

--- loaddata.R
+++
@@ -1,7 +0,0 @@
-install.packages("devtools")
-devtools::install_github("mikaelpoul/readgdf")
-library(readgdf)
-
-data <- read_gdf("channelnet_seeds1_nodes35_2019_04_29-09_11_49.gdf")
-head(data)
-data
