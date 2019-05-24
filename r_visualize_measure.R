4a8053a834c18c6dfce7fdcb713770b9074d8dcc
Sally Isaacoff <sallyisa@unity3d.com>
2019-05-24 12:14:54 +0200

    fix betweenness, add correlation plot

--- r_visualize_measure.R
+++ r_visualize_measure.R
@@ -5,11 +5,13 @@ rm(list=ls())
 
 ### loading packages
 install.packages('RColorBrewer')
+install.packages('corrplot')
+library(corrplot)
+if (!require("ForceAtlas2")) devtools::install_github("analyxcompany/ForceAtlas2")
 library(RColorBrewer)
 library(igraph)
 library(dplyr)
 library("ggplot2")
-if (!require("ForceAtlas2")) devtools::install_github("analyxcompany/ForceAtlas2")
 library("ForceAtlas2")
 setwd("/Users/sallyisa/Documents/school/social-network-analysis/")
 
@@ -41,14 +43,15 @@ load_channel_graph <- function(metadata, rels, graph_name){
                                                               k = 400, gravity=1, ks=0.1, ksmax=10, delta = 1,  
                                                               center=NULL, tolerance = 0.1, dim = 2,
                                                               plotstep=10, plotlabels=TRUE))
-  legend("topleft",legend=levels(as.factor(df_meta$is_inappropriate)), 
-         pch=20 , pt.cex = 3, cex = 1.5, 
-         col=coul, bty = "n", text.col=coul)
+  legend("bottomleft", title='Is inappropriate:', title.col = 'black',
+         legend=levels(as.factor(df_meta$is_inappropriate)), 
+         pt.cex = 3, cex = 0.9,
+         col=coul, fill=coul, text.col=coul)
   g
   }
 
 
-
+# visualize channels associated with inappropriate video network videos
 g <-load_channel_graph('annotated_channels_gdf_metadata.csv','annotated_channels_gdf_relations.csv', 'Annotated Inappropriate Channel Graph')
 V(g)$is_inappropriate
 
@@ -97,6 +100,7 @@ load_video_graph <- function(metadata,rels, graph_name){
                                           k = 400, gravity=1, ks=0.1, ksmax=10, delta = 1,  
                                           center=NULL, tolerance = 0.1, dim = 2,
                                           plotstep=10, plotlabels=TRUE))
+  
   }
   df_meta$title_language[match(V(g)$name,df_meta$label)] 
   g
@@ -120,8 +124,10 @@ construct_degree_results_df <- function(){
                           degree_norm=rep(NA, N),
                           indegree_norm=rep(NA, N), 
                           outdegree_norm=rep(NA, N), 
-                          #betweenness=rep(NA, N),
-                          closeness=rep(NA,N), stringsAsFactors=FALSE)
+                          betweenness_norm=rep(NA, N),
+                          closeness_norm=rep(NA,N), 
+                          coreness=rep(NA,N),
+                          stringsAsFactors=FALSE)
   }
 
 
@@ -136,7 +142,7 @@ get_cliques <- function(graph, df_meta){
        vertex.color = "white")
   
   ### identifying cliques
-  clique_size <-10
+  clique_size <-20
   cat('\ncalculating cliques')
   cliques <- cliques(graph, min =clique_size) #mininum clique size = 5 nodes
   cat('\ncliques calculated')
@@ -175,80 +181,56 @@ get_cliques <- function(graph, df_meta){
   
 }
 
-get_kcores <- function(graph){
+get_kcores <- function(graph, n, outname){
   
   ### computing graph coreness
   V(graph)$Kcore = graph.coreness(graph)
   
-  # examining the graph
-  plot(graph,
-       main="K Cores",
-       vertex.size = 20,
-       vertex.label = V(graph)$Kcore,
-       vertex.label.color = "black",
-       vertex.label.cex = 1,
-       vertex.color = "white",
-       edge.arrow.size = 0.1)
+  # storing a list of nodes that belong to k < n
+  exclude <- V(graph)[Kcore < n]
   
-  # storing a list of nodes that belong to k <=3
-  exclude <- V(graph)[Kcore < 4]
+  # removing these nodes in order to make a k-n core
+  kn <- delete.vertices(graph, exclude)
   
-  # removing these nodes in order to make a k-4 core
-  k4 <- delete.vertices(graph, exclude)
-  
-  ### plotting k4
-  plot(k4,
-       main="K Cores",
-       vertex.size = 20,
-       vertex.label = V(k4)$Kcore,
-       vertex.label.color = "black",
-       vertex.label.cex = 1,
-       vertex.color = "white",
-       edge.arrow.size = 0.1)
-  
-  ### plotting k4 where color reflects k
-  plot(k4,
-       main="K Cores",
-       vertex.size = 20,
-       vertex.label = V(k4)$Kcore,
-       vertex.label.color = "black",
-       vertex.label.cex = 1,
-       vertex.color = V(graph)$Kcore,
-       edge.arrow.size = 0.1)
-  
-  ### plotting k4 where node size reflects
-  #computing degree
-  deg <- degree(k4)
-  plot(k4,
-       main="K Cores",
+  ### plotting k-n where label reflect k-n, color reflects k, node size reflects degree
+  #  compute degree
+  deg <- degree(kn)
+  plot(kn,
+       main=paste(outname,", K Cores: k =", n),
        vertex.size = deg, # node size reflects degree
-       vertex.label = V(k4)$Kcore,
+       vertex.label = V(kn)$Kcore,
        vertex.label.color = "black",
        vertex.label.cex = 1,
-       vertex.color = V(graph)$Kcore,
+       vertex.color = V(kn)$Kcore,
        edge.arrow.size = 0.1)
+  V(graph)$Kcore
 }
+
 
 visualize_ggplot <- function(df.g, outname){
   df.g$deg_group[df.g$is_inappropriate == 'YES'] <- "Is inappropriate"
   df.g$deg_group[df.g$is_inappropriate == 'NO'] <- "Is child-friendly"
-  ggplot(df.g, aes(indegree_norm, outdegree_norm, size=closeness, colour=deg_group)) + geom_point() + labs(x ="Normalized In-degree", y ="Normalized Out-degree")
+  ggplot(df.g, aes(indegree_norm, outdegree_norm, size=closeness_norm, colour=deg_group)) + geom_point() + labs(x ="Normalized In-degree", y ="Normalized Out-degree")
   ggsave(paste(outname, "_norm_indegree_norm_outdegree.png"), plot =last_plot(),width =25, height =25, units ="cm",dpi =1000)
   ggplot(df.g, aes(indegree, outdegree),colour=deg_group) + geom_point() + labs(x ="In-degree", y ="Out-degree")
   ggsave(paste(outname, "_indegree_outdegree.png"), plot =last_plot(),width =25, height =25, units ="cm",dpi =1000)
-  ggplot(df.g, aes(degree, closeness),colour=deg_group) + geom_point() + labs(x ="degree", y ="closeness")
+  ggplot(df.g, aes(degree, closeness_norm),colour=deg_group) + geom_point() + labs(x ="degree", y ="closeness")
   ggsave(paste(outname, "_degree_closeness.png"), plot = last_plot(),width =25, height =25, units ="cm",dpi =1000)
 }
 
 # todo, add viewcount to node level output
-get_measures <- function(metadata, rels, outname){
+get_measures <- function(metadata, rels, outname, k){
   cat('Running measures for', outname, '...')
   ## loading the data
   df_meta <- read.csv(metadata)
+  
+  # get summary statistics of metadata
   outfile <- paste(outname, "statistics.txt")
   sink(file=outfile)
   print(summary(df_meta))
   sink()
+  
+  # load relations
   df <- read.csv(rels) 
   df <- data.frame(df$node1, df$node2) # remove index column
   ## creating a graph object
@@ -262,12 +244,12 @@ get_measures <- function(metadata, rels, outname){
                      degree = degree(g, mode = "all", normalized = F), # raw degree
                      indegree = degree(g, mode = "in", normalized = F ), # raw indegree
                      outdegree = degree(g, mode = "out", normalized = F ), # raw outdegree
-                     #betweenness = betweenness(g,  directed = F, normalized = T), # normalized betweenness
-                     closeness = closeness(g, mode = "all", normalized = T) # normalized closeness
+                     betweenness_norm = betweenness(g,  directed = T, normalized = T), # normalized betweenness
+                     closeness_norm = closeness(g, mode = "all", normalized = T), # normalized closeness
+                     coreness = coreness(g)
                      ) 
-  
-  #cat('\ncloseness score filtered', g[inappropriate_vertices])
-  df.g$is_inappropriate = df_meta$is_inappropriate[match(df.g$video,df_meta$label)] # join to metadata 
+  df.g$is_inappropriate = df_meta$is_inappropriate[match(df.g$video,df_meta$label)] # join graph to metadata: is_inappropriate
+  df.g$view_count = df_meta$viewCount.INT[match(df.g$video,df_meta$label)] # join graph to metadata: number of views
   # ranking the videos by raw (non-normalized) indegree in descending order
   df.g <- df.g %>% arrange(-indegree)
   ### exporting the dataframe as a csv in your working directory
@@ -280,8 +262,9 @@ get_measures <- function(metadata, rels, outname){
                 mean(df.g[['degree_norm']]), 
                 mean(df.g[['indegree_norm']]),
                 mean(df.g[['outdegree_norm']]),
-                #mean(df.g[['betweenness']]),
-                mean(df.g[['closeness']]))
+                mean(df.g[['betweenness_norm']], na.rm=TRUE),
+                mean(df.g[['closeness_norm']]),
+                mean(df.g[['coreness']]))
   out_measures[2, ] <- list("aggregate", "median",
                             median(df.g[['degree']]), 
                             median(df.g[['indegree']]), 
@@ -289,8 +272,9 @@ get_measures <- function(metadata, rels, outname){
                             median(df.g[['degree_norm']]), 
                             median(df.g[['indegree_norm']]),
                             median(df.g[['outdegree_norm']]),
-                            #median(df.g[['betweenness']]),
-                            median(df.g[['closeness']]))
+                            median(df.g[['betweenness_norm']], na.rm=TRUE),
+                            median(df.g[['closeness_norm']]),
+                            median(df.g[['coreness']]))
   
   # print network measures
   cat('\nfull network measures:')
@@ -313,8 +297,10 @@ get_measures <- function(metadata, rels, outname){
                             mean(df_childfriendly_centrality[['degree_norm']]), 
                             mean(df_childfriendly_centrality[['indegree_norm']]),
                             mean(df_childfriendly_centrality[['outdegree_norm']]),
-                            #mean(df_childfriendly_centrality[['betweennness']]),
-                            mean(df_childfriendly_centrality[['closeness']]))
+                            mean(df_childfriendly_centrality[['betweenness_norm']], na.rm=TRUE),
+                            mean(df_childfriendly_centrality[['closeness_norm']]),
+                            mean(df_childfriendly_centrality[['coreness']])
+                            )
   out_measures[4, ] <- list("child friendly videos", "median", 
                             median(df_childfriendly_centrality[['degree']]), 
                             median(df_childfriendly_centrality[['indegree']]), 
@@ -322,8 +308,10 @@ get_measures <- function(metadata, rels, outname){
                             median(df_childfriendly_centrality[['degree_norm']]), 
                             median(df_childfriendly_centrality[['indegree_norm']]),
                             median(df_childfriendly_centrality[['outdegree_norm']]),
-                            #median(df_childfriendly_centrality[['betweennness']]),
-                            median(df_childfriendly_centrality[['closeness']]))
+                            median(df_childfriendly_centrality[['betweenness_norm']], na.rm=TRUE),
+                            median(df_childfriendly_centrality[['closeness_norm']]),
+                            median(df_childfriendly_centrality[['coreness']])
+                            )
   out_measures[5, ] <- list("inappropriate videos", "mean", 
                             mean(df_inapprop_centrality[['degree']]), 
                             mean(df_inapprop_centrality[['indegree']]), 
@@ -331,8 +319,10 @@ get_measures <- function(metadata, rels, outname){
                             mean(df_inapprop_centrality[['degree_norm']]), 
                             mean(df_inapprop_centrality[['indegree_norm']]),
                             mean(df_inapprop_centrality[['outdegree_norm']]),
-                            #mean(df_inapprop_centrality[['betweennness']]),
-                            mean(df_inapprop_centrality[['closeness']]))
+                            mean(df_inapprop_centrality[['betweenness_norm']], na.rm=TRUE),
+                            mean(df_inapprop_centrality[['closeness_norm']]),
+                            mean(df_inapprop_centrality[['coreness']])
+                            )
   out_measures[6, ] <- list("inappropriate videos", "median", 
                             median(df_inapprop_centrality[['degree']]), 
                             median(df_inapprop_centrality[['indegree']]), 
@@ -340,28 +330,42 @@ get_measures <- function(metadata, rels, outname){
                             median(df_inapprop_centrality[['degree_norm']]), 
                             median(df_inapprop_centrality[['indegree_norm']]),
                             median(df_inapprop_centrality[['outdegree_norm']]),
-                            #median(df_inapprop_centrality[['betweennness']]),
-                            median(df_inapprop_centrality[['closeness']]))
+                            median(df_inapprop_centrality[['betweenness_norm']], na.rm=TRUE),
+                            median(df_inapprop_centrality[['closeness_norm']]),
+                            median(df_inapprop_centrality[['coreness']])
+                            )
+  # visualize centrality measures in ggplot
   visualize_ggplot(df.g, outname)
-  
   }
   cat('\nIn/out degree measures:\n')
   out_measures <- na.omit(out_measures) # return measure, drop null rows 
   print(out_measures, row.names = FALSE)
-  # get_cliques(g, df_meta)
-  get_kcores(g)
-  # visualize centrality measures in ggplot
+  get_cliques(g, df_meta)
+  # k_cores = get_kcores(g, k, outname)
+  # get correlations
+  df.g$is_inappropriate_num <- !as.integer(as.character(df.g$is_inappropriate)=="YES")
+  df.g$is_inappropriate_num <- !as.integer(as.character(df.g$is_inappropriate)=="NO")
+  df.g$is_inappropriate_num[is.na(df.g$is_inappropriate_num)] <- 0
+  df.g$view_count[is.na(df.g$view_count)] <- 0
+  M <-cor(df.g[, c(2,3,4,5,6,7,8,9,10,12,13)])
+  corrplot(M, type="upper", order="hclust",
+           col=brewer.pal(n=8, name="RdYlBu"))
   df.g
 }
 
 data_bad_measures <-get_measures('annotated_videonet_seeds_elsa_spiderman_2019_08_18_metadata.csv',
-                          'videonet_seeds_elsa_spiderman_2019_08_18_relations.csv', 'inappropriate_video_net')
-data_bad_measures <- data_bad_measures %>% arrange(-closeness)  # sort by column name
+                          'videonet_seeds_elsa_spiderman_2019_08_18_relations.csv', 
+                          'inappropriate_video_net',
+                          k=4)
+data_bad_measures[, c(2,3,4,5,6,7,8,9,10,12,13)]
+data_bad_measures
+data_bad_measures <- data_bad_measures %>% arrange(-betweenness_norm)  # sort by column name
 write.csv(data_bad_measures, 'video_node_measures-inappropriate_network.csv')
 
 data_good_measures <-get_measures('masha_and_shark_2019_05_14_metadata.csv',
-                                       'masha_and_shark_2019_05_14_relations.csv', 
-                                       'child_friendly_video_net')
+                                  'masha_and_shark_2019_05_14_relations.csv', 
+                                  'child_friendly_video_net',
+                                  k=30)
 write.csv(data_good_measures, 'video_node_measures-child_friendly_network.csv')
 
 ggplot(data_good_measures, aes(indegree_norm, outdegree)) + geom_point() + labs(x ="Normalized In-degree", y ="Normalized Out-degree")
